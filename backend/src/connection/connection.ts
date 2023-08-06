97b57b7aac0e60ebe109850b6542886acbe78eab
DevNachoGarcia <igarciariestra@gmail.com>
2023-08-06 09:53:16 -0300

    added backend/src/connection/default_user.json added backend/src/connection/fake_default_user_notes.json updated backend/ts.config.json - enabled resolveJsonModule updated backend/src/connection/connection.ts - added default_user and his fake_notes creation on connectionDB function

--- backend/src/connection/connection.ts
+++ backend/src/connection/connection.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { Note } from "../models/note.model";
 import { User } from "../models/user.model";
+import default_user from "./default_user.json"
+import fake_default_user_notes from "./fake_default_user_notes.json"
 
 export const connection = new Sequelize({
   dialect: "postgres",
@@ -23,6 +25,8 @@ User.hasMany(Note, {
 async function connectionDB() {
   try {
     await connection.sync({force: true});
+    await User.create(default_user)
+    await Note.bulkCreate(fake_default_user_notes)
   } catch (error) {
     console.log(error);
   }
