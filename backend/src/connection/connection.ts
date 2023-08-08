f4f495094e13d6ddda71960e3fd5d77801420a26
DevNachoGarcia <igarciariestra@gmail.com>
2023-08-08 20:19:27 -0300

    updated backend/src/connection/connection.ts - updated the way to make the initial auto seed to be able to register the default user with the hashed password without generating error in Note_fk

--- backend/src/connection/connection.ts
+++ backend/src/connection/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
 import { Note } from "../models/note.model";
 import { User } from "../models/user.model";
-import default_user from "./default_user.json"
-import fake_default_user_notes from "./fake_default_user_notes.json"
+import default_user from "./default_user.json";
+import fake_default_user_notes from "./fake_default_user_notes.json";
+import bcrypt from "bcrypt";
 
 export const connection = new Sequelize({
   dialect: "postgres",
@@ -11,22 +12,27 @@ export const connection = new Sequelize({
   password: "",
   database: "note_this_db",
   logging: false,
-  models: [
-    User,
-    Note
-  ],
+  models: [User, Note],
 });
 
 User.hasMany(Note, {
   foreignKey: "user_id",
-  sourceKey: "id"
-})
+  sourceKey: "id",
+});
 
 async function connectionDB() {
   try {
-    await connection.sync({force: true});
-    await User.create(default_user)
-    await Note.bulkCreate(fake_default_user_notes)
+    await connection.sync({ force: true });
+
+    const { firstname, lastname, email, password } = default_user;
+    bcrypt.hash(password, 12).then((hash) => {
+      User.create({
+        firstname,
+        lastname,
+        email,
+        password: hash,
+      }).then(() => Note.bulkCreate(fake_default_user_notes));
+    });
   } catch (error) {
     console.log(error);
   }
